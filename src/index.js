ad9db05571ed70fab165beaec25216712365d885
dmitriypavlovskyi <pavlovskyidmitriy@gmail.com>
2018-03-12 10:00:13 +0200

    Webpack dev server added

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './style.scss';
 
 class Image extends React.Component {
+  constructor(props) {
+    super()
+    this.state = {
+      title: "Granite Peaks"
+    }
+  }
   render() {
     return (
       <div>
-        <h1>{this.props.title}</h1>
+        <h1>{this.state.title}</h1>
         <p>{this.props.caption}</p>
         <div id="image"></div>
       </div>
@@ -14,6 +20,5 @@ class Image extends React.Component {
   }
 }
 
-ReactDOM.render(<Image title="Desolation Wilderness"
-                       caption="Wilderness area"/>,
+ReactDOM.render(<Image caption="Wilderness area"/>,
                        document.getElementById('container'));
